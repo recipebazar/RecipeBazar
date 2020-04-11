e01c8c8e2f58ee909b4f4b683e8d95af42704ccc
RecipeBazar <recipebazarofficial@gmail.com>
2020-04-11 10:33:46 -0500

    adding git pages

--- RecipeBazar/src/app/app.module.ts
+++ RecipeBazar/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RecipesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
