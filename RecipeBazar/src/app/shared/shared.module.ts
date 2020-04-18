9f13922e66db24e746c8197b3be1b13f26677c2a
Ramyasri Martha <ramyasri.martha@dimensional.com>
2020-04-18 18:17:21 -0500

    removed paperkit

--- RecipeBazar/src/app/shared/shared.module.ts
+++ RecipeBazar/src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    TopNavBarComponent
   ]
 })
 export class SharedModule { }
